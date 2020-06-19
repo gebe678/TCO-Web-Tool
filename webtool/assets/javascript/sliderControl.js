cb4cf43f8610c97c739745e3d6e56f2655ffcb57
gebe678 <lehrerg6@gmail.com>
2020-06-19 17:15:24 -0400

    added sliders to the detailed view page

--- webtool/assets/javascript/sliderControl.js
+++ webtool/assets/javascript/sliderControl.js
@@ -1,13 +1,23 @@
+// main function called after window content is loaded
+// All other functions are called from here
 function main()
 {
-    let slider = document.querySelector(".slider");
-    let text = document.querySelector(".outputText");
+    let slider = document.querySelectorAll(".slider");
+    let text = document.querySelectorAll(".outputText");
 
-    text.innerHTML = slider.value;
-
-    slider.oninput = function()
+    for(let i = 0; i < slider.length; i++)
     {
-        text.innerHTML = this.value;
+         slider[i].value = text[i].value;
+
+         text[i].oninput = function()
+         {
+             slider[i].value = text[i].value;
+         }
+
+         slider[i].oninput = function()
+         {
+             text[i].value = slider[i].value;
+         }
     }
 }
 
