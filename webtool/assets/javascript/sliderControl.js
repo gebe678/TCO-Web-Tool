9bde8c9e112cf491da548eab6b2fbce9ef778c12
gebe678 <lehrerg6@gmail.com>
2020-06-19 18:47:15 -0400

    fixed slider display issue

--- webtool/assets/javascript/sliderControl.js
+++ webtool/assets/javascript/sliderControl.js
@@ -4,7 +4,14 @@ function main()
 {
     let slider = document.querySelectorAll(".slider");
     let text = document.querySelectorAll(".outputText");
+    let textBlock = document.querySelectorAll(".textBlock");
 
+    syncSliderAndText(slider, text);
+    lineElements(textBlock);
+}
+
+function syncSliderAndText(slider, text)
+{
     for(let i = 0; i < slider.length; i++)
     {
          slider[i].value = text[i].value;
@@ -19,6 +26,37 @@ function main()
              text[i].value = slider[i].value;
          }
     }
+}
+
+function lineElements(textBlock)
+{
+    let cDifference = longestBlock(textBlock);
+
+    for(let i = 0; i < textBlock.length; i++)
+    {
+        let diff = cDifference - textBlock[i].innerHTML.length;
+
+        for(let j = 0; j < diff; j++)
+        {
+            // find an invisible character that will move everything onward
+            textBlock[i].innerHTML = textBlock[i].innerHTML + "\r";
+        }
+    }
+}   
+
+function longestBlock(textBlock)
+{
+    let characterDifference = textBlock[0].innerHTML.length;
+
+    for(let i = 0; i < textBlock.length; i++)
+    {
+        if(textBlock[i].innerHTML.length > characterDifference)
+        {
+            characterDifference = textBlock[i].innerHTML.length;
+        }
+    }
+
+    return characterDifference;
 }
 
 window.addEventListener("DOMContentLoaded", function(){
