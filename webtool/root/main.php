bde9072e37f79dab0425aab592d1a09929932ff4
gebe678 <lehrerg6@gmail.com>
2020-11-04 17:31:00 -0500

    fixed github issue

--- webtool/root/main.php
+++ webtool/root/main.php
@@ -138,6 +138,21 @@
             <input type="number" min="0" max="1" value=".001" step=".001" class="outputText" id="downPaymentNumber">
         </div>
 
+        <div class="dropDownMenu">
+                    <div class="label">
+                        <label for="insuranceType">Insruance Type:</label>
+                    </div>
+                    <div class="border">
+                        <select name="insuranceType" class="selectMenu" id="insuranceType">
+                            <option value="average">Average</option>
+                            <option value="min">Minimum</option>
+                            <option value="max">Maximum</option>
+                            <option value="SD1+">SD1+</option>
+                            <option value="SD1-">SD1-</option>
+                        </select>
+                    </div>
+                </div>
+
         <!--End of the new values to be moved into appropiate categories!!!!!-->
 
             <div class="simplifiedView">
